35ef43d0009590560dd1d28ed6e3334f43493ad7
soniczxc <139926954+soniczxc@users.noreply.github.com>
2024-02-13 18:23:59 +0300

    Update DeviceA.cpp

--- src/parallel_executor/Device/DeviceA.cpp
+++ src/parallel_executor/Device/DeviceA.cpp
@@ -1,9 +1,22 @@
 #include "DeviceA.h"
+#include <string>
+#include <cstdlib>
 
+char * generator(int len){
+    char symbols[27]  = "abcdefghijklmnopqrstuvwxyz";
+    char *output = new char[len];
+    int random;
+    int i;
+    for (i = 0; i<len; i++) {
+        random = rand()%(26);
+        output[i] = symbols[random];
+    }
+    return output;
+}
 std::string DeviceA::getName() {
     return "A";
 }
 
 std::string DeviceA::getDataAsString() {
-    return "Hello world!";
+    return generator(rand()%501);
 }
