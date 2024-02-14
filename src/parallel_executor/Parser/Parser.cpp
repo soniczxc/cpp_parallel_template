63e1797f1cdf548e1a82d6909ca3f001dd18f16d
Vladimir Grablyk <vov.2018@gmail.com>
2024-02-14 11:37:38 +0300

    queue use

--- src/parallel_executor/Parser/Parser.cpp
+++ src/parallel_executor/Parser/Parser.cpp
@@ -4,51 +4,44 @@
 #include "parallel_executor/Event/DataEvent.h"
 #include <thread>
 #include <chrono>
-
 Parser::Parser(std::shared_ptr<EventQueue> queue, std::shared_ptr<Device> A, std::shared_ptr<Device> B)
         : queue(queue), A(A), B(B) {
-        if (!queue || !A || !B){
-            throw std::runtime_error("Can't be nullptr!");
-        }
+    if (!queue || !A || !B){
+        throw std::runtime_error("Can't be nullptr!");
+    }
 }
 
 void Parser::read(std::shared_ptr<Device> device, std::chrono::seconds sleep_duration, size_t loop_count, int crush_index) {
-    //std::cout << StartedEvent(device).toString() << std::endl << std::flush;
-    if(queue){
-        queue->push(std::make_shared<StartedEvent>(device));
-    }
+    queue->push(std::make_shared<StartedEvent>(device)); // Помещаем событие StartedEvent в очередь
+
     for (size_t i = 0; i < loop_count; ++i) {
-        auto event = queue->pop(std::chrono::seconds(0));
-        if (event){
-            std::cout<<event->toString()<<std::endl<<std::flush;
+        std::shared_ptr<const Event> event = queue->pop(std::chrono::seconds(0)); // Извлекаем сообщение из очереди
+        if (event != nullptr) {
+            std::cout << event->toString() << std::endl << std::flush;
         }
+
         if (i == crush_index) {
             std::this_thread::sleep_for(std::chrono::seconds(1));
-            //queue->push(DataEvent(device));
-            queue->push(std::make_shared<DataEvent>("Device " + device->getName() + " stopped"));
-            std::cout << "Device " << device->getName() << " stopped" << std::endl << std::flush;
+            queue->push(std::make_shared<WorkDoneEvent>(device));
             break;
         }
+
         std::this_thread::sleep_for(sleep_duration);
         auto data = device->getDataAsString();
-        //queue->push(std::make_shared<DataEvent>(device->getDataAsString()));
-        std::cout << DataEvent(device).toString() << std::endl << std::flush;
-
-       
+        queue->push(std::make_shared<DataEvent>(device)); // Помещаем событие DataEvent в очередь
     }
 
     if (crush_index == -1) {
         queue->push(std::make_shared<WorkDoneEvent>(device));
-        //std::cout << WorkDoneEvent(device).toString() << std::endl;
     }
 }
 
-void Parser::run(size_t loop_count_A, size_t loop_count_B, int crush_index_A, int crush_index_B) {
 
+
+
+void Parser::run(size_t loop_count_A, size_t loop_count_B, int crush_index_A, int crush_index_B) {
     std::thread threadA(&Parser::read, this, A, std::chrono::seconds(1), loop_count_A, crush_index_A);
     std::thread threadB(&Parser::read, this, B, std::chrono::seconds(5), loop_count_B, crush_index_B);
-
     threadA.join();
     threadB.join();
 }
-
