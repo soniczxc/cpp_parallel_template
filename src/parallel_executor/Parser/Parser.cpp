c3eb6bfd19962d113b9d44b3d225fa54e7f634f1
vova <vov.2018@gmail.com>
2024-02-28 15:43:37 +0300

    some changes

--- src/parallel_executor/Parser/Parser.cpp
+++ src/parallel_executor/Parser/Parser.cpp
@@ -1,55 +1,56 @@
 #include "Parser.h"
-#include "parallel_executor/Event/StartedEvent.h"
+#include "parallel_executor/Event/DeviceEvent.h"
 #include "parallel_executor/Event/WorkDoneEvent.h"
 #include "parallel_executor/Event/DataEvent.h"
 #include <thread>
-#include <chrono>
-Parser::Parser(std::shared_ptr<EventQueue> queue, std::shared_ptr<Device> A, std::shared_ptr<Device> B)
-        : queue(queue), A(A), B(B) {
-    if (!queue || !A || !B){
-        throw std::runtime_error("Can't be nullptr!");
-    }
-}
-void PrintQueue(std::shared_ptr<EventQueue> queue){
-    std::shared_ptr<const Event> event;
+#include <iostream>
 
-    while (queue) {
-        event = queue->pop(std::chrono::seconds(0));
-        if (event != nullptr) {
-            std::cout << event->toString() << std::endl << std::flush;
-        }
-    }
-}
-void Parser::read(std::shared_ptr<Device> device, std::chrono::seconds sleep_duration, size_t loop_count, int crush_index) {
-    //std::cout << StartedEvent(device).toString() << std::endl << std::flush;
-    queue->push(std::make_shared<StartedEvent>(device));
-
-    for (size_t i = 0; i < loop_count; ++i) {
-        std::this_thread::sleep_for(sleep_duration);
-        //auto data = device->getDataAsString();
-        //std::cout << DataEvent(device).toString() << std::endl << std::flush;
-        queue->push(std::make_shared<DataEvent>(device));
-        PrintQueue(queue);
-        if (i == loop_count -1) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            queue->push(std::make_shared<WorkDoneEvent>(device));
-            //std::cout << WorkDoneEvent(device).toString() << std::endl << std::flush;
-            PrintQueue(queue);
-            break;
-        }
-        PrintQueue(queue);
-    }
-    if (crush_index == -1 ) {
-        //std::cout << WorkDoneEvent(device).toString() << std::endl << std::flush;
-        queue->push(std::make_shared<WorkDoneEvent>(device));
-        PrintQueue(queue);
-    }
-
-}
+Parser::Parser(std::shared_ptr<EventQueue> queue, std::shared_ptr<Device> A, std::shared_ptr<Device> B) : queue(queue), A(A), B(B) {}
 
 void Parser::run(size_t loop_count_A, size_t loop_count_B, int crush_index_A, int crush_index_B) {
-    std::thread threadA(&Parser::read, this, A, std::chrono::seconds(1), loop_count_A, crush_index_A);
-    std::thread threadB(&Parser::read, this, B, std::chrono::seconds(5), loop_count_B, crush_index_B);
+    bool deviceAStopped = false;
+    bool deviceBStopped = false;
+
+    std::thread threadA(&Parser::read, this, A, std::chrono::seconds(1), loop_count_A, crush_index_A, std::ref(deviceAStopped));
+    std::thread threadB(&Parser::read, this, B, std::chrono::seconds(5), loop_count_B, crush_index_B, std::ref(deviceBStopped));
+
     threadA.join();
     threadB.join();
+
+    /*
+    bool bothDevicesStopped = false;
+    while (!bothDevicesStopped) {
+        if (deviceAStopped && deviceBStopped) {
+            bothDevicesStopped = true;
+        } else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+    */
+
+    //if (deviceAStopped && deviceBStopped) {
+        if (crush_index_A == -1 || crush_index_A >= loop_count_A) {
+            queue->push(std::make_shared<WorkDoneEvent>(A));
+        }
+        if (crush_index_B == -1 || crush_index_B >= loop_count_B) {
+            queue->push(std::make_shared<WorkDoneEvent>(B));
+        }
 }
+
+
+void Parser::read(std::shared_ptr<Device> device, std::chrono::seconds sleep_duration, size_t loop_count, int crush_index, bool& deviceStopped) {
+    for (size_t i = 0; i < loop_count; ++i) {
+        if (crush_index != -1 && i == crush_index) {
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            std::cout << "Device " << device->getName() << " stopped" << std::endl;
+            deviceStopped = true;
+            return;
+        }
+
+        std::this_thread::sleep_for(sleep_duration);
+        queue->push(std::make_shared<DataEvent>(device));
+    }
+
+    deviceStopped = true;
+}
+
--- src/parallel_executor/Parser/Parser.h
+++ src/parallel_executor/Parser/Parser.h
@@ -1,46 +1,23 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <memory>
+#include <chrono>
 #include "parallel_executor/EventQueue/EventQueue.h"
 #include "parallel_executor/Device/Device.h"
-#include "stdexcept"
-class Parser
-{
+
+class Parser {
 public:
-  /*!
-   * \brief Конструктор
-   *
-   * \param queue Умный указатель на очередь.
-   * \param A Умный указатель на устройство A.
-   * \param B Умный указатель на устройство B.
-  */
-
     Parser(std::shared_ptr<EventQueue> queue, std::shared_ptr<Device> A, std::shared_ptr<Device> B);
-  /*!
-   * \brief Функция, запускающая чтение данных с устройств в параллельном режиме.
-   *
-   * \param loop_count_A Беззнаковое целое число, задающее кол-во итераций чтения с устройства А.
-   * \param loop_count_B Беззнаковое целое число, задающее кол-во итераций чтения с устройства В.
-   * \param crush_index_A Целое число, указывающая на какой итерации сломается устройство А.
-   * \param crush_index_B Целое число, указывающая на какой итерации сломается устройство B.
-  */
 
-  void run(size_t loop_count_A = 1, size_t loop_count_B = 1, int crush_index_A = -1, int crush_index_B = -1);
-private:
-  /*!
-   * \brief Читает данные с устройства.
-   *
-   * \param device Умный указатель на устройство
-   * \param sleep_duration Секунды, задающие длительность прерывания для симулированной работы.
-   * \param loop_count Беззнаковое целое число, задающее кол-во итераций чтения с устройства.
-   * \param crush_index Целое число, указывающая на какой итерации сломается устройство.
-  */
-  void read(std::shared_ptr<Device> device, std::chrono::seconds sleep_duration, size_t loop_count, int crush_index);
+    void run(size_t loop_count_A, size_t loop_count_B, int crush_index_A, int crush_index_B);
 
 private:
-  std::shared_ptr<EventQueue> queue;
-  std::shared_ptr<Device> A;
-  std::shared_ptr<Device> B;
+    void read(std::shared_ptr<Device> device, std::chrono::seconds sleep_duration, size_t loop_count, int crush_index, bool& deviceStopped);
+
+    std::shared_ptr<EventQueue> queue;
+    std::shared_ptr<Device> A;
+    std::shared_ptr<Device> B;
 };
 
 #endif
